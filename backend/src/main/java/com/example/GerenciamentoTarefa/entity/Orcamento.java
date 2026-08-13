5fb071d0cb6c542c55eb94caa12dc6824a789a6e
alanhiran <alanhiranpereiragularte@gmail.com>
2026-08-13 20:28:23 -0300

    feat entity

---
+++ backend/src/main/java/com/example/GerenciamentoTarefa/entity/Orcamento.java
@@ -0,0 +1,25 @@
+package com.example.GerenciamentoTarefa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Orcamento {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String titulo;
+   private String descricao;
+   private LocalDate dataVencimento;
+   private StatusTarefa status;
+
+}
---
+++ backend/src/main/java/com/example/GerenciamentoTarefa/entity/StatusTarefa.java
@@ -0,0 +1,9 @@
+package com.example.GerenciamentoTarefa.entity;
+
+public enum StatusTarefa {
+
+    PENDENTE,
+    EM_ANDAMENTO,
+    CONCLUIDA,
+
+    }
