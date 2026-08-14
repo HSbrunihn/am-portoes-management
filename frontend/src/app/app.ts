e37c26942d954025f54a5367d771858ac499885e
Brunihn <veniciosbruno5@gmail.com>
2026-08-14 01:05:27 -0300

    feat: cria componente task-card e exibe  dados mockados

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,13 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Header } from './components/header/header';
+import { TaskCard } from './components/task-card/task-card';
+import { TASKS_MOCK } from './mock-data/tasks.mock';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Header],
+  imports: [RouterOutlet, Sidebar, Header, TaskCard],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('am-portoes-frontend');
+  protected readonly tasks = TASKS_MOCK;
 }
